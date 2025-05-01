19649429b033463f23089bc26d897db251b5c7a6
imR4V3N <antonio.rajaonarimino@gmail.com>
2025-05-01 09:28:27 +0300

    NewApp

---
+++ src/main/java/mg/erp/S6Evaluation2ErpJavaApplication.java
@@ -0,0 +1,13 @@
+package mg.erp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S6Evaluation2ErpJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S6Evaluation2ErpJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mg/erp/S6Evaluation2ErpJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package mg.erp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S6Evaluation2ErpJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
